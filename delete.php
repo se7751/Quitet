8880ecce1634cf5c05b4f3b2c8599d80a6e54bc8
picasa <picasa@picasa-pc>
2014-08-28 22:01:33 +0900

    ファイル削除処理の追加

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ $userId = getUserID ();
 $db = DBManager::instance ();
 
 
-if(isset($_GET['project_id'])){
+if(isset($_GET['project_id'])){//プロジェクトを消す
 	echo "PROJECT DELETE";
 	$query = <<<EOT
 	DELETE
@@ -21,7 +21,7 @@ EOT;
 	$url = sprintf('Location: %s', "board.php");
 	header($url);
 	exit;
-}elseif (isset($_GET['ticket_id'])){
+}elseif (isset($_GET['ticket_id']) && !isset($_GET['file_name'])){//チケットを消す
 	echo "TICKET DELETE";
 	$query = <<<EOT
 	DELETE
@@ -35,7 +35,23 @@ EOT;
 	$url = sprintf('Location: %s', "viewTickets.php?project_id=".$_SESSION ['ch_project_id']);
 	header($url);
 	exit;
-}
+}elseif (isset($_GET['ticket_id']) && isset($_GET['file_name'])){//チケットのファイルを消す
+	echo "TICKET FILE DELETE";
+	$query = <<<EOT
+	UPDATE
+		tickets
+	SET
+		file_name =""
+	where
+		ticket_id = '%s'
+EOT;
+	$query = sprintf ( $query, $_GET['ticket_id'] );
+	$db->exec ( $query );
+	unlink("view/".$_GET['file_name']);
+	$url = sprintf('Location: %s', "viewTicket.php?ticket_id=".$_SESSION ['ch_ticket_id']);
+	header($url);
+	exit;
+}//コメントのファイルを消す
 
 /*
 
